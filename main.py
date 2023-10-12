c33c1b9f511dae882d3fe9cf06d583279a51bde9
Diego Restrepo <restrepo@udea.edu.co>
2023-10-12 18:14:22 -0500

    beta version

--- main.py
+++ main.py
@@ -22,13 +22,18 @@ app.mount("/static", StaticFiles(directory="static"), name="static") ###
 @app.get("/")
 def read_item(concept: str = "", output: str = "html"):
     '''
-    You can write the API documentation here:
+    You can write the API documentation (http://127.0.0.1:8000/docs ) here: 
     
     For example: 
     
     USAGE: http://127.0.0.1:8000/?concept=particle+physics
+    
+    or 
+    
+    http://127.0.0.1:8000/?concept=particle+physics
     '''
-    #Real time JSON file:
+    
+    # Get the OpenAlex level 1 concept id
     concept_id=''
     if concept:
         c=requests.get(f'https://api.openalex.org/concepts?filter=display_name.search:{concept}')
@@ -36,12 +41,14 @@ def read_item(concept: str = "", output: str = "html"):
         
         concept_id=f"concept.id:{c.split('/')[-1]},"
  
-        
+    
+    # Real time OpenAlex JSON file:
     file=f'https://api.openalex.org/sources?filter={concept_id}apc_usd:0'
     
+    # (1) ETL: Extract Transform and Load into `db`
     r=requests.get(file)
     rr=r.json().get('results')
-    maxj=100
+    
     db=[
         { 
           'journal'   : d.get('display_name'),
@@ -49,16 +56,19 @@ def read_item(concept: str = "", output: str = "html"):
           'articles'  : f"{d.get('works_count'):,}",
           'citations' : f"{d.get('cited_by_count'):,}",
           'h_index'   : d.get('summary_stats').get('h_index')
-         } for d in rr[:maxj]
+         } for d in rr
     ]
-    print('*',concept,'*',db)
+    
+    # (2) BACKEND
     f=open('static/data/filtered.json','w')
     json.dump(db,f)
     f.close()
 
     if output == 'json':
+        # (2) BACKEND EndPoint 
         # http://127.0.0.1:8000/?concept=particle+physics&output=json
         return db
     else:
+        # (3) FRONTEND
         # http://127.0.0.1:8000/?concept=particle+physics
         return FileResponse('index.html')
